6ede0b6c01ce2022c29abff4989678bb4b1d374c
GaoFei <strivek@163.com>
2014-12-22 15:36:40 +0800

    更新导航栏active同步

--- docApp/javascript/main.js
+++ docApp/javascript/main.js
@@ -1,8 +1,20 @@
-docApp.controller('RouteListCtrl', function ($scope) {
-});
-docApp.controller('RouteNavCtrl', function ($scope, $routeParams) {
+docApp.controller('ListCtrl', function($scope) {
+    var selectedCategory = "home";
 
+    $scope.selectedCategory = function(newCategory) {
+        selectedCategory = newCategory;
+    }
+    $scope.getCategoryClass = function(item) {
+    	console.log(item,selectedCategory);
+        return item == selectedCategory ? "active" : "";
+    }
 });
-docApp.controller('RouteHomeCtrl', function ($scope, $routeParams) {
-
+docApp.controller('RouteNavCtrl',function(){
+	
+});
+docApp.controller('RouteListCtrl',function(){
+	
+});
+docApp.controller('RouteHomeCtrl',function(){
+	
 });
